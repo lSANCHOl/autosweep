85baf54ca0b66d3e0a27ff69b5cca2f57af18435
lSANCHOl <60328198+lSANCHOl@users.noreply.github.com>
2020-12-14 18:06:53 +0000

    Update autosweep.sh
    simplified the code and made it work with all IP addresses now.

--- autosweep.sh
+++ autosweep.sh
@@ -1,64 +1,16 @@
 #!/bin/bash
 
 
-
-
-me2=$(ifconfig | grep 192 | cut -d " " -f 10)
-
+me2=$(ifconfig | grep broadcast | cut -d " " -f 10)
+echo "[+Sancho's Ping Sweeper+]"
 echo "YOUR IP:" ${me2}
 
-
-
-if [ ${#me2} == 16 ]
-then
-	theip=$(echo ${me2::-6})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-if [ ${#me2} == 15 ]
-then
-	theip=$(echo ${me2::-5})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-if [ ${#me2} == 14 ]
-then
-	theip=$(echo ${me2::-4})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-
-
-if [ ${#me2} == 11 ]
-then
-	theip=$(echo ${me2::-1})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-if [ ${#me2} == 12 ]
-then
-	theip=$(echo ${me2::-2})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-if [ ${#me2} == 13 ]
-then
-	theip=$(echo ${me2::-3})
-	echo ${theip}
-	echo "ALL DEVICE IPv4:"
-fi
-
-
+theip=${me2%.*}
+echo ${theip}
 
 for ip in `seq 1 254`; do 
-ping -c 1 ${theip}$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ping -c 1 ${theip}"."$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
 
 echo "ALL MAC ADDRESSES:"
 arp -a | grep :
-
